572a55585f2998f43897d7d265bedfb55051ecee
Akash <phalke.aka@gmail.com>
2023-02-05 15:58:26 +0530

    update

--- 2version.tf
+++ 2version.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">=1.2.0"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "4.53.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
