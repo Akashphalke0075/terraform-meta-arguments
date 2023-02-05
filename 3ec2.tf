58da41100522d09e36904ed9117e506c5d130031
Akash <phalke.aka@gmail.com>
2023-02-05 15:52:27 +0530

    update

--- 3ec2.tf
+++ 3ec2.tf
@@ -4,11 +4,11 @@ resource "aws_instance" "class-ec2-server" {
     depends_on = [security_group.class-ec2-server-sg]
     vpc_security_group_ids = [security_group.class-ec2-server-sg.id]
     tags {
-        "Name" = "class-ec2-server"
+        "Name" = "5class-ec2-server"
     }
 }
 
 resource "security_group" "class-ec2-server-sg" {
-    "Name" = "class-ec2-server-sg"
+    name = "class-ec2-server-sg"
     description = "Allow SSH access"
 }
