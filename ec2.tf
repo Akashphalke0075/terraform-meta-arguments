7a181c6f37a9e8974061ff5ca3af65328c93552d
Akash <phalke.aka@gmail.com>
2023-02-05 16:02:30 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "class-ec2-server" {
   depends_on = [aws_security_group.class-ec2-server-sg] # Meta argument
   vpc_security_group_ids = [aws_security_group.class-ec2-server-sg.id]
   tags = {
-    "Name" = "1class-ec2-server"
+    "Name" = "class-ec2-server"
   }
 }
 
@@ -16,20 +16,20 @@ resource "aws_security_group" "class-ec2-server-sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["106.213.72.234/32"] #Change IP Address as per your need
+    cidr_blocks = ["94.213.52.135/32"] #Change IP Address as per your need
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["106.213.72.234/32"] #Change IP Address as per your need
+    cidr_blocks = ["94.213.52.135/32"] #Change IP Address as per your need
   }
   egress {
-    cidr_blocks = [ "106.213.72.234/32" ]
+    cidr_blocks = [ "0.0.0.0/0" ]
     ipv6_cidr_blocks = ["::/0"]
     from_port = 0
     to_port   = 0
     protocol = -1
   } 
-# }
+}
