bce1a2d61f4b2ddf9745bb98ac515981a0bc1961
Akash <phalke.aka@gmail.com>
2023-02-05 15:50:35 +0530

    update

---
+++ 3ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "class-ec2-server" {
+    instance_type = "t2.micro"
+    ami = "ami-0b5eea76982371e91"
+    depends_on = [security_group.class-ec2-server-sg]
+    vpc_security_group_ids = [security_group.class-ec2-server-sg.id]
+    tags {
+        "Name" = "class-ec2-server"
+    }
+}
+
+resource "security_group" "class-ec2-server-sg" {
+    "Name" = "class-ec2-server-sg"
+    description = "Allow SSH access"
+}
