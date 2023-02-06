f38c4d8ccad362084d80a37230fd3ed045b30f7e
Akash <phalke.aka@gmail.com>
2023-02-07 00:30:31 +0530

    update

--- 2ec2.tf
+++ 2ec2.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "newclass-ec2-server" {
   instance_type = "t2.micro"
   ami = "ami-0b5eea76982371e91"
-  depends_on = [aws_security_group.newclass-ec2-server-sg]
-  vpc_security_group_ids = [aws_security_group.newclass-ec2-server-sg.id]
-  count = 3
-  tags = {
-    Name = "newclass-ec2-server-$[count.index]"
-  }
+  # depends_on = [aws_security_group.newclass-ec2-server-sg]
+  # vpc_security_group_ids = [aws_security_group.newclass-ec2-server-sg.id]
+  # count = 3
+  # tags = {
+  #   Name = "newclass-ec2-server-$[count.index]"
+  # }
 }
 
 
@@ -15,11 +15,17 @@ resource "aws_security_group" "newclass-ec2-server-sg" {
   description = "Allow SSH Access"
 
 
-ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["106.213.72.234/32"] #Change IP Address as per your need
-  }
+# ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["106.213.72.234/32"] #Change IP Address as per your need
+#   }
 
+}
+
+resource "aws_instance" "newclass-ec2-server-eu" {
+  instance_type = "t2.micro"
+  ami = "ami-0b5eea76982371e91"
+  provider = aws.aws-eu-region
 }
