92ec10c0ecc7c1031ba3b1ea513f5d1fff438eb3
Akash <phalke.aka@gmail.com>
2023-02-08 18:10:29 +0530

    update

--- 3ec2.tf
+++ 3ec2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "best-class-ec2" {
     ami = "ami-0b5eea76982371e91"
     instance_type = "t2.micro"
-    depends_on = "aws_security_group.best-sec-ec2-sg"
-    vpc_security_group_ids = "aws_security_group.best-sec-ec2-sg.id"
+    depends_on = "aws_security_group.best-sec-ec2-sg1"
+    vpc_security_group_ids = "aws_security_group.best-sec-ec2-sg1.id"
     tags = {
         Name = "first-instance"
     }
 }
 
-resource "aws_security_group" "best-sec-ec2-sg" {
-    name = "best-sec-ec2-sg"
+resource "aws_security_group" "best-sec-ec2-sg1" {
+    name = "best-sec-ec2-sg1"
     description = "Allow SSH Access"
 
 ingress {
