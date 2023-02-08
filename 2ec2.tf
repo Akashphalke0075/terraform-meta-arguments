c3735f730790694cc0b5fc423e67453bc271b229
Akash <phalke.aka@gmail.com>
2023-02-08 18:06:29 +0530

    update

--- 2ec2.tf
+++ 2ec2.tf
@@ -1,31 +1,31 @@
-resource "aws_instance" "newclass-ec2-server" {
-  instance_type = "t2.micro"
-  ami = "ami-0b5eea76982371e91"
-  # depends_on = [aws_security_group.newclass-ec2-server-sg]
-  # vpc_security_group_ids = [aws_security_group.newclass-ec2-server-sg.id]
-  # count = 3
-  # tags = {
-  #   Name = "newclass-ec2-server-$[count.index]"
-  # }
-}
+# resource "aws_instance" "newclass-ec2-server" {
+#   instance_type = "t2.micro"
+#   ami = "ami-0b5eea76982371e91"
+#   # depends_on = [aws_security_group.newclass-ec2-server-sg]
+#   # vpc_security_group_ids = [aws_security_group.newclass-ec2-server-sg.id]
+#   # count = 3
+#   # tags = {
+#   #   Name = "newclass-ec2-server-$[count.index]"
+#   # }
+# }
 
 
-resource "aws_security_group" "newclass-ec2-server-sg" {
-  name = "newclass-ec2-server-sg"
-  description = "Allow SSH Access"
+# resource "aws_security_group" "newclass-ec2-server-sg" {
+#   name = "newclass-ec2-server-sg"
+#   description = "Allow SSH Access"
 
 
-# ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["106.213.72.234/32"] #Change IP Address as per your need
-#   }
+# # ingress {
+# #     from_port   = 22
+# #     to_port     = 22
+# #     protocol    = "tcp"
+# #     cidr_blocks = ["106.213.72.234/32"] #Change IP Address as per your need
+# #   }
 
-}
+# }
 
-resource "aws_instance" "newclass-ec2-server-eu" {
-  instance_type = "t2.micro"
-  ami = "ami-0fe0b2cf0e1f25c8a"
-  provider = aws.aws-eu-region
-}
+# resource "aws_instance" "newclass-ec2-server-eu" {
+#   instance_type = "t2.micro"
+#   ami = "ami-0fe0b2cf0e1f25c8a"
+#   provider = aws.aws-eu-region
+# }
---
+++ 3ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "best-class-ec2" {
+    ami = "ami-0b5eea76982371e91"
+    instance_type = "t2.micro"
+    depends_on = "aws_security_group.best-class-ec2-sg"
+    vpc_security_group_ids = "aws_security_group.best-class-ec2-sg.id"
+    tags = {
+        Name = "first-instance"
+    }
+}
+
+resource "aws_security_group" "best-class-ec2-sg" {
+    name = "best-class-ec2-sg"
+    description = "Allow SSH Access"
+
+ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["106.213.72.234"]
+}
+
+}
