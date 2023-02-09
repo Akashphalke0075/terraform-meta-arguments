caf5fa63241fc07d8db04100c15da1dff6151b76
Akash <phalke.aka@gmail.com>
2023-02-09 18:36:08 +0530

    update

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "class-us-vpc" {
-cidr_block = "32.52.62.22/22"
+cidr_block = "10.10.80.0/22"
 }
 
 resource "aws_vpc" "class-europe-vpc" {
-cidr_block = "32.52.54.12/22"
+cidr_block = "10.10.80.0/22"
 provider = aws.aws-eu-region
 }
